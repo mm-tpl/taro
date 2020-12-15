70f06268a0dd4f3d114f87c1fd81cbc35e007243
taoqf <tao_qiufeng@126.com>
2020-12-15 20:56:52 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+export default {
+	pages: [
+		'pages/pg001/pg001'
+	],
+	window: {
+		backgroundTextStyle: 'light',
+		navigationBarBackgroundColor: '#fff',
+		navigationBarTitleText: 'WeChat',
+		navigationBarTextStyle: 'black'
+	}
+}
---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.scss'
+
+export default function App({ children }) {
+	return (
+		<>
+			{children}
+		</>
+	);
+}
