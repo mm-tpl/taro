1ddf4155f75a86f78d593bd698418cb5b573b3fc
taoqf <tao_qiufeng@126.com>
2020-12-25 14:19:23 +0800

    editor config

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,7 +5,7 @@ export default {
 	window: {
 		backgroundTextStyle: 'light',
 		navigationBarBackgroundColor: '#fff',
-		navigationBarTitleText: 'WeChat',
+		navigationBarTitleText: 'Mmstudio',
 		navigationBarTextStyle: 'black'
 	}
 }
--- src/pages/pg001/pg001.tsx
+++ src/pages/pg001/pg001.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { } from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
+// import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './pg001.scss'
 
 export default function index() {
 	return (
 		<>
-			<View className='index'>
-				<Text>Hello world!</Text>
-				<AtButton type='primary'>I need Taro UI</AtButton>
-				<Text>Taro UI 支持 Vue 了吗？</Text>
-				<AtButton type='primary' circle={true}>支持</AtButton>
-				<Text>共建？</Text>
-				<AtButton type='secondary' circle>来</AtButton>
+			<View>
+				<Text>mmstudio!</Text>
 			</View>
 		</>
 	);
