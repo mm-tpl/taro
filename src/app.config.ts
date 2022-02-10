45a3e1ef19a63052807df1f5bff4ab8fb5690ecd
taoqf <tao_qiufeng@126.com>
2022-02-10 14:26:31 +0800

    add config

--- src/app.config.ts
+++ src/app.config.ts
@@ -10,5 +10,27 @@ export default {
 		navigationBarTitleText: 'Mmstudio',
 		navigationBarTextStyle: 'black',
 		backgroundColor: '#fff'
-	}
+	},
+	// tabBar: {
+	// 	color: '#413E3E',
+	// 	selectedColor: '#FF8181',
+	// 	backgroundColor: '#6360FF',
+	// 	list: [{
+	// 		pagePath: 'pages/home/home',
+	// 		iconPath: './tabBar/home.png',
+	// 		selectedIconPath: './tabBar/home1.png',
+	// 		text: '首页'
+	// 	},
+	// 	{
+	// 		pagePath: 'pages/todo/todo',
+	// 		iconPath: './tabBar/daiban.png',
+	// 		selectedIconPath: './tabBar/daiban1.png',
+	// 		text: '待办'
+	// 	}, {
+	// 		pagePath: 'pages/my/my',
+	// 		iconPath: './tabBar/my.png',
+	// 		selectedIconPath: './tabBar/my1.png',
+	// 		text: '我的'
+	// 	}]
+	// }
 } as taro.Config;
